6960a06f09f6be6d4fa30f9bec0b8437d5342c79
Adam <amarinelli@esri.ca>
2014-11-02 21:51:24 -0500

    Adding Full Calendar with Google calendar source

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+  $('#calendar').fullCalendar({
+      events: 'https://www.google.com/calendar/feeds/en.canadian%23holiday%40group.v.calendar.google.com/public/basic'
+  });
+});
