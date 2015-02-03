a0172fc356d198751dd2dcda86c818bdb99ef614
Adam <amarinelli@esri.ca>
2015-02-03 00:03:26 -0500

    Updated pages and content

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,24 @@
+$( document ).ready(function() {
+  console.log("ready!");
+  $("#mainContent").load("pages/home.html");
+});
 
-$(document).ready(function(){
+//Home nav link
+$("#nav1").click(function() {
+  $("#mainContent").load("pages/home.html");
+});
 
+//SNC2D nav link
+$("#nav2").click(function() {
+  $("#mainContent").load("pages/snc2d.html");
+});
+
+//Textbooks nav link
+$("#nav3").click(function() {
+  $("#mainContent").load("pages/textbooks.html");
+});
+
+//Links nav link
+$("#nav4").click(function() {
+  $("#mainContent").load("pages/links.html");
 });
